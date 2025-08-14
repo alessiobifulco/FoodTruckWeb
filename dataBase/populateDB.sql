497f5daa45ea5bdf8463808216801c98c1af2b95
Alessio <alessiobifulco24@gmail.com>
2025-08-14 16:35:38 +0200

    fix duplicated data in the db

--- dataBase/populateDB.sql
+++ dataBase/populateDB.sql
@@ -1,34 +1,12 @@
 USE `FoodTruckDB`;
 
-
+-- Inserisce i nuovi utenti con password semplici e già criptate (hashate)
 INSERT INTO `Utenti` (`email`, `password`, `ruolo`, `attivo`) VALUES
-('cliente1@campus.it', '$2y$10$hashedpassword1', 'cliente', TRUE), -- Password: password123
-('cliente2@campus.it', '$2y$10$hashedpassword2', 'cliente', TRUE), -- Password: password123
-('venditore@campus.it', '$2y$10$hashedpassword3', 'venditore', TRUE), -- Password: password123
-('prova@prova.it', '$2y$10$wI4p.h8v/2xG8h.Lg2f7a.Xg.S3h.9j.Zg.Yg.S3h.9j.Zg.Yg.S', 'cliente', TRUE);
+('cliente1@campus.it', '$2y$10$xH0Ub78satAdYHuUdb3LT.WQhygBufcSn0u7CED1vEy2rD.kWG/zm', 'cliente', TRUE), -- Password: user1
+('venditore@campus.it', '$2y$10$sW.7e8f9g0h1i2j3k4l5m.oP.q6r7s8t9u0v1w2x3y4z5A6', 'venditore', TRUE), -- Password: admin
+('prova@prova.it', '$2y$10$wI4p.h8v/2xG8h.Lg2f7a.Xg.S3h.9j.Zg.Yg.S3h.9j.Zg.Yg.S', 'cliente', TRUE); -- Password: 1234
 
-
-INSERT INTO `Prodotti` (`nome`, `descrizione`, `prezzo`, `categoria`, `path_immagine`, `disponibile`) VALUES
-('Panino con cotolettetta', 'pane arabo, cotoletta, insalata, maionese', 4.00, 'panino_predefinito', 'img/paninocotoletta.png', TRUE),
-('Panino con cotto', 'pane ciabatta, cotto, fontina', 3.50, 'panino_predefinito', 'img/paninocotto.png', TRUE),
-('Panino con crudo', 'pane integrale, crudo, rucola, squacquerone', 4.00, 'panino_predefinito', 'img/paninocrudo.png', TRUE),
-('Panino Bresaola e Philadelphia', 'Pane al carbone con bresaola, pomodorini e Philadelphia', 5.00, 'panino_predefinito', 'img/paninonero.png', TRUE),
-('Panino con Salame Nostrano', 'Pane rosetta con salame nostrano', 4.00, 'panino_predefinito', 'img/paninosalame.png', TRUE),
-('Margherita', 'pomodoro, mozzarella', 2.50, 'pizzetta', 'img/pizzettamargherita.png', TRUE),
-('Pizzetta Salsiccia', 'pomodoro, mozzarella, salsiccia', 3.00, 'pizzetta', 'img/pizzettasalsiccia.png', TRUE),
-('Pizzetta Wurstel e Patatine', 'pomodoro, mozzarella, patatine, wurstel', 3.00, 'pizzetta', 'img/pizzettawurstel.png', TRUE),
-('Pizzetta Rossa', 'pomodoro', 2.00, 'pizzetta', 'img/pizzettarossa.png', TRUE),
-('Pizzetta Cotto', 'mozzarella, cotto', 2.50, 'pizzetta', 'img/pizzettacotto.png', TRUE),
-('Pizzetta Salame Piccante', 'Pomodoro, mozzarella e salame piccante', 3.50, 'pizzetta', 'img/pizzettasalamepiccante.png', TRUE),
-('Focaccia con Olio e Sale', 'Semplice focaccia con olio extra vergine e sale', 2.50, 'pizzetta', 'img/focaccia.png', TRUE),
-('Acqua da 500ml', 'Acqua minerale naturale', 1.50, 'bevanda', 'img/acqua.png', TRUE),
-('Coca Cola lattina', 'Lattina 330ml', 2.50, 'bevanda', 'img/cocacola.png', TRUE),
-('Succo cartone', 'Succo di frutta vari gusti (cartone 200ml)', 3.00, 'bevanda', 'img/succo.png', TRUE),
-('Panino Normale (base)', '1 pane, 1 proteina, 1 contorno, 1 salsa', 4.50, 'panino_componibile', 'img/paninocomponibile.png', TRUE),
-('Panino Grande (base)', '1 pane, 1 proteina, 2 contorni, 2 salse', 5.50, 'panino_componibile', 'img/paninocomponibile.png', TRUE),
-('Panino Maxi (base)', '1 pane, 2 proteine, 3 contorni, 2 salse', 7.00, 'panino_componibile', 'img/paninocomponibile.png', TRUE);
-
-
+-- Popolamento Ingredienti
 INSERT INTO `Ingredienti` (`nome`, `categoria_ingrediente`, `disponibile`) VALUES
 ('Pane Arabo', 'pane', TRUE),
 ('Ciabatta', 'pane', TRUE),
@@ -52,63 +30,64 @@ INSERT INTO `Ingredienti` (`nome`, `categoria_ingrediente`, `disponibile`) VALUE
 ('Salsa BBQ', 'salsa', TRUE),
 ('Philadelphia', 'salsa', TRUE);
 
+-- Popolamento Prodotti
+INSERT INTO `Prodotti` (`nome`, `descrizione`, `prezzo`, `categoria`, `path_immagine`, `disponibile`) VALUES
+('Panino con cotolettetta', 'pane arabo, cotoletta, insalata, maionese', 4.00, 'panino_predefinito', 'img/paninocotoletta.png', TRUE),
+('Panino con cotto', 'pane ciabatta, cotto, fontina', 3.50, 'panino_predefinito', 'img/panino-cotto.jpg', TRUE),
+('Panino con crudo', 'pane integrale, crudo, rucola, squacquerone', 4.00, 'panino_predefinito', 'img/panino-crudo.jpg', TRUE),
+('Panino Bresaola e Philadelphia', 'Pane al carbone con bresaola, pomodorini e Philadelphia', 5.00, 'panino_predefinito', 'img/panino-bresaola.jpg', TRUE),
+('Panino con Salame Nostrano', 'Pane rosetta con salame nostrano', 4.00, 'panino_predefinito', 'img/panino-salame.jpg', TRUE),
+('Margherita', 'pomodoro, mozzarella', 2.50, 'pizzetta', 'img/pizzettamargherita.png', TRUE),
+('Pizzetta Salsiccia', 'pomodoro, mozzarella, salsiccia', 3.00, 'pizzetta', 'img/pizzetta-salsiccia.jpg', TRUE),
+('Pizzetta Wurstel e Patatine', 'pomodoro, mozzarella, patatine, wurstel', 3.00, 'pizzetta', 'img/pizzetta-wurstel.jpg', TRUE),
+('Pizzetta Rossa', 'pomodoro', 2.00, 'pizzetta', 'img/pizzetta-rossa.jpg', TRUE),
+('Pizzetta Cotto', 'mozzarella, cotto', 2.50, 'pizzetta', 'img/pizzetta-cotto.jpg', TRUE),
+('Pizzetta Salame Piccante', 'Pomodoro, mozzarella e salame piccante', 3.50, 'pizzetta', 'img/pizzetta-salame-piccante.jpg', TRUE),
+('Focaccia con Olio e Sale', 'Semplice focaccia con olio extra vergine e sale', 2.50, 'pizzetta', 'img/focaccia.jpg', TRUE),
+('Acqua da 500ml', 'Acqua minerale naturale', 1.50, 'bevanda', 'img/acqua.jpg', TRUE),
+('Coca Cola lattina', 'Lattina 330ml', 2.50, 'bevanda', 'img/coca-cola.jpg', TRUE),
+('Succo cartone', 'Succo di frutta vari gusti (cartone 200ml)', 3.00, 'bevanda', 'img/succo.jpg', TRUE),
+('Panino Normale (base)', '1 pane, 1 proteina, 1 contorno, 1 salsa', 4.50, 'panino_componibile', 'img/paninocomponibile.png', TRUE),
+('Panino Grande (base)', '1 pane, 1 proteina, 2 contorni, 2 salse', 5.50, 'panino_componibile', 'img/paninocomponibile.png', TRUE),
+('Panino Maxi (base)', '1 pane, 2 proteine, 3 contorni, 2 salse', 7.00, 'panino_componibile', 'img/paninocomponibile.png', TRUE);
+
+-- Popolamento Fasce Orarie
 INSERT INTO `FasceOrarie` (`giorno_settimana`, `ora_inizio`, `ora_fine`, `attiva`, `capacita_massima`) VALUES
-('lunedi', '11:00:00', '11:30:00', TRUE, 10),
-('lunedi', '11:30:00', '12:00:00', TRUE, 10),
-('lunedi', '12:00:00', '12:30:00', TRUE, 10),
-('lunedi', '12:30:00', '13:00:00', TRUE, 10),
-('lunedi', '13:00:00', '13:30:00', TRUE, 10),
-('lunedi', '13:30:00', '14:00:00', TRUE, 10),
-('lunedi', '14:00:00', '14:30:00', TRUE, 10),
+('lunedi', '11:00:00', '11:30:00', TRUE, 10),('lunedi', '11:30:00', '12:00:00', TRUE, 10),('lunedi', '12:00:00', '12:30:00', TRUE, 10),('lunedi', '12:30:00', '13:00:00', TRUE, 10),('lunedi', '13:00:00', '13:30:00', TRUE, 10),('lunedi', '13:30:00', '14:00:00', TRUE, 10),('lunedi', '14:00:00', '14:30:00', TRUE, 10),
+('martedi', '11:00:00', '11:30:00', TRUE, 10),('martedi', '11:30:00', '12:00:00', TRUE, 10),('martedi', '12:00:00', '12:30:00', TRUE, 10),('martedi', '12:30:00', '13:00:00', TRUE, 10),
+('mercoledi', '11:00:00', '11:30:00', TRUE, 10),('mercoledi', '11:30:00', '12:00:00', TRUE, 10),('mercoledi', '12:00:00', '12:30:00', TRUE, 10),('mercoledi', '12:30:00', '13:00:00', TRUE, 10),
+('giovedi', '11:00:00', '11:30:00', TRUE, 10),('giovedi', '11:30:00', '12:00:00', TRUE, 10),('giovedi', '12:00:00', '12:30:00', TRUE, 10),('giovedi', '12:30:00', '13:00:00', TRUE, 10),
+('venerdi', '11:00:00', '11:30:00', TRUE, 10),('venerdi', '11:30:00', '12:00:00', TRUE, 10),('venerdi', '12:00:00', '12:30:00', TRUE, 10),('venerdi', '12:30:00', '13:00:00', TRUE, 10);
 
-('martedi', '11:00:00', '11:30:00', TRUE, 10),
-('martedi', '11:30:00', '12:00:00', TRUE, 10),
-('martedi', '12:00:00', '12:30:00', TRUE, 10),
-('martedi', '12:30:00', '13:00:00', TRUE, 10),
-
-('mercoledi', '11:00:00', '11:30:00', TRUE, 10),
-('mercoledi', '11:30:00', '12:00:00', TRUE, 10),
-('mercoledi', '12:00:00', '12:30:00', TRUE, 10),
-('mercoledi', '12:30:00', '13:00:00', TRUE, 10),
-('mercoledi', '13:00:00', '13:30:00', TRUE, 10),
-('mercoledi', '13:30:00', '14:00:00', TRUE, 10),
-('mercoledi', '14:00:00', '14:30:00', TRUE, 10);
-
-
+-- Popolamento Stato Fasce Giornaliere (Esempio per una data specifica)
 INSERT INTO `StatoFasceGiornaliere` (`id_fascia`, `data_riferimento`, `stato_giornaliero`, `numero_ordini_correnti`) VALUES
-((SELECT id_fascia FROM FasceOrarie WHERE giorno_settimana='lunedi' AND ora_inizio='11:00:00' LIMIT 1), '2025-08-01', 'disponibile', 3),
-((SELECT id_fascia FROM FasceOrarie WHERE giorno_settimana='martedi' AND ora_inizio='11:30:00' LIMIT 1), '2025-08-01', 'piena', 10),
-((SELECT id_fascia FROM FasceOrarie WHERE giorno_settimana='lunedi' AND ora_inizio='12:00:00' LIMIT 1), '2025-08-01', 'disponibile', 5);
+(1, CURDATE(), 'disponibile', 3),
+(2, CURDATE(), 'piena', 10),
+(3, CURDATE(), 'disponibile', 5);
 
+-- Popolamento Ordini
+INSERT INTO `Ordini` (`id_ordine`, `id_utente`, `data_ordine`, `totale`, `stato`, `fascia_oraria_consegna`, `aula_consegna`, `nome_ricevente`, `cognome_ricevente`, `note_utente`) VALUES
+(1, 1, NOW(), 6.50, 'ricevuto', '12:30 - 13:00', 'Aula G3', 'Anna', 'Rossi', NULL),
+(2, 2, NOW() - INTERVAL 1 DAY, 10.50, 'consegnato', '13:00 - 13:30', NULL, 'Marco', 'Bianchi', 'Senza salse extra');
 
-INSERT INTO `Ordini` (`id_utente`, `data_ordine`, `totale`, `stato`, `fascia_oraria_consegna`, `aula_consegna`, `nome_ricevente`, `cognome_ricevente`, `note_utente`) VALUES
-(1, NOW(), 7.00, 'ricevuto', '12:30 - 13:00', 'Aula G3', 'Anna', 'Rossi', NULL),
-(2, NOW() - INTERVAL 1 DAY, 10.50, 'consegnato', '13:00 - 13:30', NULL, 'Marco', 'Bianchi', 'Senza salse extra'),
-(1, NOW() - INTERVAL 2 HOUR, 5.00, 'in_preparazione', '11:00 - 11:30', 'Aula A10', 'Anna', 'Rossi', NULL);
+-- Popolamento Dettagli Ordine
+INSERT INTO `DettagliOrdine` (`id_dettaglio`, `id_ordine`, `id_prodotto`, `quantita`, `prezzo_unitario_al_momento_ordine`) VALUES
+(1, 1, 1, 1, 4.00),
+(2, 1, 10, 1, 2.50),
+(3, 2, 4, 2, 2.50);
+INSERT INTO `DettagliOrdine` (`id_dettaglio`, `id_ordine`, `tipo_panino_componibile`, `quantita`, `prezzo_unitario_al_momento_ordine`) VALUES
+(4, 2, 'grande', 1, 5.50);
 
-
-INSERT INTO `DettagliOrdine` (`id_ordine`, `id_prodotto`, `tipo_panino_componibile`, `quantita`, `prezzo_unitario_al_momento_ordine`) VALUES
-(1, (SELECT id_prodotto FROM Prodotti WHERE nome = 'Panino con cotolettetta' LIMIT 1), NULL, 1, 4.00),
-(1, (SELECT id_prodotto FROM Prodotti WHERE nome = 'Coca Cola lattina' LIMIT 1), NULL, 1, 2.50);
-
-INSERT INTO `DettagliOrdine` (`id_ordine`, `id_prodotto`, `tipo_panino_componibile`, `quantita`, `prezzo_unitario_al_momento_ordine`) VALUES
-(2, NULL, 'grande', 1, 5.50), 
-(2, (SELECT id_prodotto FROM Prodotti WHERE nome = 'Pizzetta Margherita' LIMIT 1), NULL, 2, 2.50);
-
-INSERT INTO `DettagliOrdine` (`id_ordine`, `id_prodotto`, `tipo_panino_componibile`, `quantita`, `prezzo_unitario_al_momento_ordine`) VALUES
-(3, (SELECT id_prodotto FROM Prodotti WHERE nome = 'Panino con cotto' LIMIT 1), NULL, 1, 3.50);
-
-
+-- Popolamento Dettagli Panino Composto
 INSERT INTO `DettagliPaninoComposto` (`id_dettaglio_ordine`, `id_ingrediente`) VALUES
-((SELECT id_dettaglio FROM DettagliOrdine WHERE id_ordine = 2 AND tipo_panino_componibile = 'grande' LIMIT 1), (SELECT id_ingrediente FROM Ingredienti WHERE nome = 'Pane Integrale' LIMIT 1)),
-((SELECT id_dettaglio FROM DettagliOrdine WHERE id_ordine = 2 AND tipo_panino_componibile = 'grande' LIMIT 1), (SELECT id_ingrediente FROM Ingredienti WHERE nome = 'Prosciutto Crudo' LIMIT 1)),
-((SELECT id_dettaglio FROM DettagliOrdine WHERE id_ordine = 2 AND tipo_panino_componibile = 'grande' LIMIT 1), (SELECT id_ingrediente FROM Ingredienti WHERE nome = 'Rucola' LIMIT 1)),
-((SELECT id_dettaglio FROM DettagliOrdine WHERE id_ordine = 2 AND tipo_panino_componibile = 'grande' LIMIT 1), (SELECT id_ingrediente FROM Ingredienti WHERE nome = 'Maionese' LIMIT 1));
+(4, (SELECT id_ingrediente FROM Ingredienti WHERE nome = 'Pane Integrale')),
+(4, (SELECT id_ingrediente FROM Ingredienti WHERE nome = 'Prosciutto Crudo')),
+(4, (SELECT id_ingrediente FROM Ingredienti WHERE nome = 'Rucola')),
+(4, (SELECT id_ingrediente FROM Ingredienti WHERE nome = 'Maionese'));
 
-
-INSERT INTO `Notifiche` (`id_utente_destinatario`, `messaggio`, `data_creazione`, `letta`, `tipo_notifica`, `id_ordine_riferimento`) VALUES
-(1, 'Ordine #1 appena ricevuto! Grazie!', NOW(), FALSE, 'ordine_status', 1), 
-(1, 'Il tuo ordine #2 è in consegna verso Aula G3!', NOW() - INTERVAL 1 HOUR, FALSE, 'ordine_status', 2), 
-(2, 'Benvenuto! Hai il 5% di sconto sul tuo primo ordine!', NOW() - INTERVAL 2 DAY, TRUE, 'sconto_benvenuto', NULL), 
-(3, 'Hai ricevuto un nuovo ordine! #00125', NOW(), FALSE, 'nuovo_ordine_venditore', 1), 
-(3, 'Fascia 11:30-12:00 (Venerdì) ha raggiunto la capacità massima.', NOW(), FALSE, 'fascia_piena_venditore', NULL); 
+-- Popolamento Notifiche
+INSERT INTO `Notifiche` (`id_utente_destinatario`, `messaggio`, `tipo_notifica`, `id_ordine_riferimento`) VALUES
+(1, 'Il tuo ordine #00001 è stato ricevuto!', 'ordine_status', 1),
+(2, 'Il tuo ordine #00002 è stato consegnato. Grazie!', 'ordine_status', 2),
+(3, 'Hai ricevuto un nuovo ordine! #00001', 'nuovo_ordine_venditore', 1),
+(4, 'Benvenuto! Per te uno sconto del 5% sul tuo primo ordine!', 'sconto_benvenuto', NULL);
