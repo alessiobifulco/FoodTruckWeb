d7158d105e92bc3a0dab90892df52b68be02af20
Alessio <alessiobifulco24@gmail.com>
2025-08-16 14:40:53 +0200

    fix name error

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
         <a href="whoweare.php">Chi Siamo</a>
         <a href="contacts.php">Contatti</a>
         <a href="privacy.php">Privacy Policy</a>
-        <a href="termini.php">Termini e Condizioni</a>
+        <a href="terms.php">Termini e Condizioni</a>
     </div>
     <div class="footer-bottom">
         <span>Copyright &copy; <?php echo date('Y'); ?> Food Truck. All Rights Reserved.</span>
